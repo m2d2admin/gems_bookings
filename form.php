01975bc7dce117ebf084d6615c2e3165df84acba
m2d2admin <github@m2-d2.com>
2024-05-21 16:57:21 +0200

    booking data comments

--- form.php
+++ form.php
@@ -1,9 +1,6 @@
 <?php
     define("GEMS_PLUGIN_DIR", plugin_dir_path(__FILE__));
 
-    // if (!class_exists('Gamajo_Template_Loader')) {
-        require plugin_dir_path(__FILE__) . 'templates/email_template.php';
-    // }
     $eventkey = get_query_var( 'event', 'abc123XYZ456' );
     //$eventkey = '64cbb86c14fb2';
 ?>
@@ -2609,50 +2606,4 @@
         </div>
     </div>
 </div>
-<?php
-function mail_booking_details() {
-	global $wpdb;
-	global $table_prefix;
-
-	if(isset($_POST['action']) && $_POST['action'] == 'mail_booking_details') {
-		$email_settings = get_option('mail_setting_'.get_current_user_id());
-		$booking_details = $_POST['bookingData'];
-
-		if (!$email_settings) {
-			$email_settings[] = array(
-				'emailSubject' 			=> '',
-				'emailHeader' 			=> '',
-				'emailFooter' 			=> '',
-			);
-		}
-
-		var_dump($booking_details);
-		var_dump('====================================');
-		// mail booking details
-		$name = 'Yanick';
-		$email = 'kevineasky@gmail.com';
-		$message = email_template($booking_details, $email_settings, $email, $name);
-
-		//php mailer variables
-		$from = get_option('admin_email');
-		$subject = $email_settings['email_subject'];
-		$headers = 'From: '. $from . "\r\n" .
-			'Reply-To: ' . $email . "\r\n";
-
-		// //Here put your Validation and send mail
-		add_filter('wp_mail_content_type', function( $content_type ) {
-            return 'text/html';
-		});
-		$sent = wp_mail($email, $subject, $message, $headers);
-			
-		// if($sent) {
-		// //message sent!       
-		// }
-		// else  {
-		// //message wasn't sent       
-		// }
-	}	
-}
-add_action( 'wp_ajax_mail_booking_details', 'mail_booking_details' );
-?>
                        
--- gems_bookings.php
+++ gems_bookings.php
@@ -360,6 +360,52 @@ function save_email_settings() {
 add_action( 'wp_ajax_save_email_settings', 'save_email_settings' );
 
 
+function mail_booking_details() {
+	global $wpdb;
+	global $table_prefix;
+
+	if(isset($_POST['action']) && $_POST['action'] == 'mail_booking_details') {
+		$email_settings = get_option('mail_setting_'.get_current_user_id());
+		$booking_details = $_POST['bookingData'];
+
+		if (!$email_settings) {
+			$email_settings[] = array(
+				'emailSubject' 			=> '',
+				'emailHeader' 			=> '',
+				'emailFooter' 			=> '',
+			);
+		}
+
+		var_dump($booking_details);
+		var_dump('====================================');
+		// mail booking details
+		$name = 'Yanick';
+		$email = 'kevineasky@gmail.com';
+		$message = email_template($booking_details, $email_settings, $email, $name);
+
+		//php mailer variables
+		$from = get_option('admin_email');
+		$subject = $email_settings['email_subject'];
+		$headers = 'From: '. $from . "\r\n" .
+			'Reply-To: ' . $email . "\r\n";
+
+		// //Here put your Validation and send mail
+		add_filter('wp_mail_content_type', function( $content_type ) {
+            return 'text/html';
+		});
+		$sent = wp_mail($email, $subject, $message, $headers);
+			
+		// if($sent) {
+		// //message sent!       
+		// }
+		// else  {
+		// //message wasn't sent       
+		// }
+	}	
+}
+add_action( 'wp_ajax_mail_booking_details', 'mail_booking_details' );
+add_action( 'wp_ajax_nopriv_mail_booking_details', 'mail_booking_details' );
+
 /***********************************************************************
  Template loader
  */
